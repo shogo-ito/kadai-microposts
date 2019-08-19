b76af32bde8be86d4f85948668f258cea08fea65
shogo.ito <noonslugger@i.softbank.jp>
2019-08-19 03:58:53 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jombotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
